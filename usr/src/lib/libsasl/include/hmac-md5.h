310081b5a46594d217398f53dcb747c201ebdab8
Rob Siemborski <rjs3@andrew.cmu.edu>
2003-05-13 21:34:28 +0000

    add extern "C" blocks for C++ compiles (Dave.Feldman@morganstanley.com)

--- usr/src/lib/libsasl/include/hmac-md5.h
+++ usr/src/lib/libsasl/include/hmac-md5.h
@@ -32,6 +32,10 @@ typedef struct HMAC_MD5_STATE_s {
 } HMAC_MD5_STATE;
 #endif /* _SUN_SDK */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* One step hmac computation
  *
  * digest may be same as text or key
@@ -60,5 +64,9 @@ void _sasl_hmac_md5_import(HMAC_MD5_CTX *hmac, HMAC_MD5_STATE *state);
  */
 void _sasl_hmac_md5_final(unsigned char digest[HMAC_MD5_SIZE],
 			  HMAC_MD5_CTX *hmac);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* HMAC_MD5_H */
