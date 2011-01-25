4de730180d593e54980712873914fc49e231e7cc
Ken Murchison <murch@andrew.cmu.edu>
2011-01-25 20:36:42 +0000

    Allow DIGEST-MD5 plugin to be used for client-side HTTP Digest (RFC 2617)

--- usr/src/lib/libsasl/include/saslplug.h
+++ usr/src/lib/libsasl/include/saslplug.h
@@ -291,7 +291,7 @@ typedef struct sasl_client_params {
 	/* for additions which don't require a version upgrade; set to 0 */
     const void *gss_creds;                  /* GSS credential handle */
     const sasl_channel_binding_t *cbinding; /* client channel binding */
-    void *spare_ptr3;
+    const sasl_http_request_t *http_request;/* HTTP Digest request method */
     void *spare_ptr4;
 
 	/*
