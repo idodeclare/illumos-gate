187484f5be341e2dc32d6c0b8ffa465cb0e77779
Alexey Melnikov <alexey.melnikov@isode.com>
2011-04-19 09:19:18 +0000

    Fix to build GSSAPI with Heimdal (patch from Russ Allbery from Debian)

--- usr/src/lib/sasl_plugins/gssapi/gssapi.c
+++ usr/src/lib/sasl_plugins/gssapi/gssapi.c
@@ -93,7 +93,7 @@ static const char plugin_id[] = "$Id: gssapi.c,v 1.75 2003/07/02 13:13:42 rjs3 E
 
 static const char * GSSAPI_BLANK_STRING = "";
 
-#ifndef HAVE_GSS_C_NT_HOSTBASED_SERVICE
+#if !defined(HAVE_GSS_C_NT_HOSTBASED_SERVICE) && !defined(GSS_C_NT_HOSTBASED_SERVICE)
 extern gss_OID gss_nt_service_name;
 #define GSS_C_NT_HOSTBASED_SERVICE gss_nt_service_name
 #endif
