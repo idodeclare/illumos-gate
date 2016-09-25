d137262f0f3933535bc6573e92f4f91ac2350fca
Chris Fraire <cfraire@me.com>
2016-09-25 10:26:08 -0500

    7384 list Makefiles "ok to skip" for MAKE_GITIGNORE, as found empirically

--- usr/src/tools/scripts/infer_make_gitignore.pl
+++ usr/src/tools/scripts/infer_make_gitignore.pl
@@ -71,7 +71,7 @@ use Cwd;
 use File::Basename;
 use Getopt::Std;
 our ($archdir, $cwd, $didname, $mach, %macros, $fullpath, $fullname,
-  $inclpathadj, @includes, $usrsrc, $worktext, $opt_v, $opt_n);
+  $inclpathadj, @includes, @oktoskip, $usrsrc, $worktext, $opt_v, $opt_n);
 
 BEGIN {
 	die "Error: need to be in illumos-gate root\n" if ! -d ".git";
@@ -210,9 +210,12 @@ foreach my $c ($alltext =~
 
 # if any signal to run MAKE_GITIGNORE was inferred...
 if ($force_make || @macros > 0) {
-	# warn if macro is not found--except for certain file exceptions
+	# warn if macro is not found--except for certain file exceptions--
+	# but explicitly let ptools/Makefile.bld go to "else"
 	if ($alltext !~ /^MAKE_GITIGNORE\b/m && $ARGV !~ m`/ptools/Makefile.bld$`) {
-		mywarn("\t\$(MAKE_GITIGNORE) is not defined in preprocessed $ARGV\n");
+		if (!is_makefile_ok_to_skip($ARGV)) {
+			mywarn("\t\$(MAKE_GITIGNORE) is not defined in preprocessed $ARGV\n");
+		}
 	}
 	elsif (!$opt_n) {
 		# Filter to unique macros
@@ -360,4 +363,189 @@ sub mywarn {
 		$didname = 1;
 	}
 	warn @_;
+}
+
+# some Makefiles empirically do not need a MAKE_GITIGNORE cleanup. Ignore
+# those explicitly to quiet some warnings.
+sub is_makefile_ok_to_skip {
+	my ($file) = @_;
+	# remove any leading part so $file is relative starting with usr/src/...
+	$file =~ s`^.+(?=\b usr/src \b)``x;
+	return scalar(grep { $file eq $_ } @oktoskip) > 0;
+}
+
+BEGIN {
+	my $oklist = <<'END_LIST';
+usr/src/cmd/cvcd/sparc/sun4u/Makefile
+usr/src/cmd/cvcd/sparc/Makefile
+usr/src/cmd/dcs/Makefile
+usr/src/cmd/dcs/sparc/Makefile
+usr/src/cmd/sendmail/cf/cf/Makefile
+usr/src/cmd/prtdscp/sparc/Makefile
+usr/src/cmd/prtdscp/Makefile
+usr/src/cmd/scsi/sestopo/Makefile
+usr/src/cmd/scsi/Makefile
+usr/src/cmd/scsi/smp/Makefile
+usr/src/cmd/fm/fmtopo/Makefile
+usr/src/cmd/fm/fmstat/Makefile
+usr/src/cmd/fm/schemes/Makefile
+usr/src/cmd/fm/notify/smtp-notify/Makefile
+usr/src/cmd/fm/notify/snmp-notify/Makefile
+usr/src/cmd/fm/fminject/Makefile
+usr/src/cmd/fm/fmadm/Makefile
+usr/src/cmd/fm/modules/Makefile
+usr/src/cmd/fm/modules/common/sw-diag-response/Makefile
+usr/src/cmd/fm/modules/common/Makefile
+usr/src/cmd/fm/modules/SUNW,Sun-Blade-T6320/Makefile
+usr/src/cmd/fm/modules/SUNW,Netra-T5220/Makefile
+usr/src/cmd/fm/modules/SUNW,USBRDT-5240/Makefile
+usr/src/cmd/fm/modules/SUNW,Netra-CP3060/Makefile
+usr/src/cmd/fm/modules/SUNW,Netra-T5440/Makefile
+usr/src/cmd/fm/modules/sun4v/Makefile
+usr/src/cmd/fm/modules/SUNW,SPARC-Enterprise/Makefile
+usr/src/cmd/fm/modules/SUNW,Netra-CP3260/Makefile
+usr/src/cmd/fm/modules/SUNW,SPARC-Enterprise-T5120/Makefile
+usr/src/cmd/fm/modules/SUNW,Sun-Fire-T200/Makefile
+usr/src/cmd/fm/modules/SUNW,T5140/Makefile
+usr/src/cmd/fm/modules/sun4u/Makefile
+usr/src/cmd/fm/modules/SUNW,Sun-Blade-T6300/Makefile
+usr/src/cmd/fm/Makefile
+usr/src/cmd/fm/fmdump/Makefile
+usr/src/cmd/fm/eversholt/esc/Makefile
+usr/src/cmd/fm/eversholt/eftinfo/Makefile
+usr/src/cmd/fm/eversholt/files/Makefile
+usr/src/cmd/fm/eversholt/Makefile
+usr/src/cmd/fm/ipmitopo/Makefile
+usr/src/cmd/prtdiag/sparc/Makefile
+usr/src/cmd/make/Makefile
+usr/src/cmd/make/lib/Makefile
+usr/src/cmd/scadm/sparc/Makefile
+usr/src/cmd/mdb/sparc/Makefile
+usr/src/cmd/mdb/i86xpv/Makefile
+usr/src/cmd/mdb/i86xpv/modules/Makefile
+usr/src/cmd/mdb/intel/modules/Makefile
+usr/src/cmd/mdb/sun4u/Makefile
+usr/src/cmd/mdb/sun4u/modules/unix/Makefile
+usr/src/cmd/mdb/sun4u/modules/opl/oplhwd/Makefile
+usr/src/cmd/mdb/sun4u/modules/opl/Makefile
+usr/src/cmd/mdb/sun4u/modules/lw8/Makefile
+usr/src/cmd/mdb/sun4u/modules/lw8/sgenv/Makefile
+usr/src/cmd/mdb/sun4u/modules/serengeti/sgsbbc/Makefile
+usr/src/cmd/mdb/sun4u/modules/serengeti/Makefile
+usr/src/cmd/mdb/i86pc/Makefile
+usr/src/cmd/mdb/i86pc/modules/Makefile
+usr/src/cmd/mdb/sun4v/modules/mdesc/Makefile
+usr/src/cmd/mdb/sun4v/modules/vdsk/Makefile
+usr/src/cmd/mdb/sun4v/modules/Makefile
+usr/src/cmd/mdb/sun4v/modules/errh/Makefile
+usr/src/cmd/mdb/sun4v/modules/unix/Makefile
+usr/src/cmd/mdb/sun4v/modules/ldc/Makefile
+usr/src/cmd/mdb/sun4v/Makefile
+usr/src/cmd/sgs/libelf/demo/Makefile
+usr/src/cmd/picl/plugins/common/Makefile
+usr/src/cmd/picl/plugins/sun4v/Makefile
+usr/src/cmd/picl/plugins/sun4v/lib/Makefile
+usr/src/cmd/picl/plugins/lib/Makefile
+usr/src/cmd/picl/plugins/sun4u/lib/Makefile
+usr/src/cmd/picl/plugins/sun4u/cherrystone/Makefile
+usr/src/cmd/picl/plugins/sun4u/silverstone/Makefile
+usr/src/cmd/picl/plugins/sun4u/daktari/Makefile
+usr/src/cmd/picl/plugins/sun4u/blade/Makefile
+usr/src/cmd/picl/plugins/sun4u/chicago/Makefile
+usr/src/cmd/picl/plugins/sun4u/lw2plus/Makefile
+usr/src/cmd/picl/plugins/sun4u/excalibur/Makefile
+usr/src/cmd/picl/plugins/sun4u/lw8/Makefile
+usr/src/cmd/picl/plugins/sun4u/schumacher/Makefile
+usr/src/cmd/picl/plugins/sun4u/seattle/Makefile
+usr/src/cmd/picl/plugins/sun4u/littleneck/Makefile
+usr/src/cmd/picl/plugins/sun4u/enchilada/Makefile
+usr/src/cmd/picl/plugins/sun4u/grover/Makefile
+usr/src/cmd/picl/plugins/sun4u/psvc/Makefile
+usr/src/cmd/picl/plugins/sun4u/mpxu/Makefile
+usr/src/cmd/picl/plugins/sun4u/chalupa/Makefile
+usr/src/cmd/picl/plugins/sun4u/taco/Makefile
+usr/src/cmd/picl/plugins/sun4u/boston/Makefile
+usr/src/cmd/picl/plugins/sun4u/snowbird/lib/Makefile
+usr/src/cmd/picl/plugins/sun4u/sebring/Makefile
+usr/src/cmd/picl/plugins/sun4u/ents/Makefile
+usr/src/cmd/picl/plugins/Makefile
+usr/src/cmd/picl/Makefile
+usr/src/cmd/zonestat/Makefile
+usr/src/cmd/sckmd/sparc/Makefile
+usr/src/cmd/lp/lib/Makefile
+usr/src/cmd/svr4pkg/Makefile
+usr/src/cmd/hal/addons/Makefile
+usr/src/cmd/hal/probing/Makefile
+usr/src/cmd/fs.d/udfs/Makefile
+usr/src/cmd/fs.d/pcfs/Makefile
+usr/src/cmd/fs.d/lofs/Makefile
+usr/src/cmd/fs.d/hsfs/Makefile
+usr/src/cmd/fs.d/zfs/Makefile
+usr/src/cmd/tnf/Makefile
+usr/src/cmd/abi/spectrans/Makefile
+usr/src/cmd/abi/Makefile
+usr/src/cmd/idmap/Makefile
+usr/src/cmd/syseventd/daemons/Makefile
+usr/src/cmd/syseventd/modules/Makefile
+usr/src/cmd/krb5/Makefile
+usr/src/cmd/rcap/Makefile
+usr/src/man/Makefile
+usr/src/ucblib/Makefile
+usr/src/uts/sparc/dump/Makefile
+usr/src/tools/make/Makefile
+usr/src/tools/make/lib/Makefile
+usr/src/lib/libmtmalloc/tests/Makefile
+usr/src/lib/libprtdiag/sparc/Makefile
+usr/src/lib/fm/topo/Makefile
+usr/src/lib/fm/topo/maps/Makefile
+usr/src/lib/fm/topo/modules/Makefile
+usr/src/lib/fm/topo/modules/i86pc/Makefile
+usr/src/lib/fm/topo/modules/SUNW,SPARC-Enterprise/Makefile
+usr/src/lib/fm/topo/modules/sun4u/Makefile
+usr/src/lib/fm/topo/modules/common/Makefile
+usr/src/lib/fm/topo/modules/SUNW,Sun-Fire/Makefile
+usr/src/lib/fm/topo/modules/sun4v/Makefile
+usr/src/lib/fm/topo/modules/SUNW,Sun-Fire-15000/Makefile
+usr/src/lib/mpapi/Makefile
+usr/src/lib/brand/ipkg/Makefile
+usr/src/lib/brand/labeled/Makefile
+usr/src/lib/brand/shared/Makefile
+usr/src/lib/libsmedia/plugins/Makefile
+usr/src/lib/libsmedia/Makefile
+usr/src/lib/gss_mechs/mech_dh/Makefile
+usr/src/lib/scsi/Makefile
+usr/src/lib/scsi/plugins/Makefile
+usr/src/lib/scsi/plugins/smp/Makefile
+usr/src/lib/scsi/plugins/ses/Makefile
+usr/src/lib/scsi/plugins/scsi/Makefile
+usr/src/lib/scsi/plugins/scsi/engines/Makefile
+usr/src/lib/libpcp/Makefile
+usr/src/lib/crypt_modules/Makefile
+usr/src/lib/lvm/libpreen/Makefile
+usr/src/lib/librsc/Makefile
+usr/src/lib/librsc/sparc/Makefile
+usr/src/lib/libsecdb/help/Makefile
+usr/src/psm/stand/cpr/sparcv9/Makefile
+usr/src/psm/stand/lib/promif/sparcv9/ieee1275/Makefile
+usr/src/psm/stand/bootlst/sparc/Makefile
+usr/src/psm/stand/bootblks/ufs/Makefile
+usr/src/psm/stand/bootblks/ufs/sparc/Makefile
+usr/src/psm/stand/bootblks/hsfs/Makefile
+usr/src/psm/stand/bootblks/zfs/Makefile
+usr/src/test/util-tests/Makefile
+usr/src/test/util-tests/tests/Makefile
+usr/src/test/os-tests/tests/Makefile
+usr/src/test/os-tests/Makefile
+usr/src/test/libc-tests/Makefile
+usr/src/test/test-runner/Makefile
+usr/src/test/test-runner/stf/Makefile
+usr/src/test/Makefile
+usr/src/test/zfs-tests/Makefile
+usr/src/test/zfs-tests/cmd/Makefile
+usr/src/test/zfs-tests/tests/functional/Makefile
+usr/src/test/zfs-tests/tests/stress/Makefile
+usr/src/test/zfs-tests/tests/Makefile
+END_LIST
+
+	push @oktoskip, split /\n/, $oklist;
 }
