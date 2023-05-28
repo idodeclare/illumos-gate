38d0f42c8ae1301a0bdf0daf9acb902fecb207b7
Chris Fraire <cfraire@me.com>
2023-05-27 20:28:30 -0500

    XXX 7384 Add scripts for creation of descendant .gitignore files

--- usr/src/tools/scripts/infer_make_gitignore.pl
+++ usr/src/tools/scripts/infer_make_gitignore.pl
@@ -104,6 +104,10 @@ undef $inclpathadj;
 # exceptions found empirically
 if ($mname eq "Makefile") {
 	# ok, no path adjustment necessary
+} elsif ($mname eq "Makefile.digest") {
+	# usr/src/test/crypto-tests/tests/digest/Makefile directive:
+	#     $(MAKE) -f Makefile.digest
+	# ok, no path adjustment necessary
 } elsif ($mname eq "Makefile.solaris") {
 	# usr/src/grub/... directive: $(MAKE) -f Makefile.solaris
 	# ok, no path adjustment necessary
